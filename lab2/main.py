e9750905b24b7868ad3a8c12d3386a38b5dbf99c
kacienk <ktcienkosz@gmail.com>
2024-03-11 08:52:05 +0100

    essential files

---
+++ lab2/main.py
@@ -0,0 +1,9 @@
+from solution import *
+
+
+blocks1dom = create_blocks_world({'a','b','c'})
+blocks1 = Planning_problem(blocks1dom,
+     {on('a'):'table', clear('a'):True,
+      on('b'):'c',  clear('b'):True,
+      on('c'):'table', clear('c'):False}, # initial state
+     {on('a'):'b', on('c'):'a'})  #goal
