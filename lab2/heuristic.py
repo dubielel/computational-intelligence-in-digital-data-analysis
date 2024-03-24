4ec6be6dba23c5636e81905acd71a31a4d5a5002
kacienk <ktcienkosz@gmail.com>
2024-03-24 15:04:26 +0100

    clean heuristic

--- lab2/heuristic.py
+++ lab2/heuristic.py
@@ -7,59 +7,45 @@ class BlocksWorldHeuristic():
     def __init__(self, problem: Planning_problem) -> None:
         self.expected_columns = self._calculate_columns(problem.goal)
         self.expected_fundaments = self._calculate_fundaments(problem.goal)
-
-
-    def _calculate_columns(self, goal) -> Dict[str, int]:
+        
+    def _get_unique_blocks(self, goal):
         unique_blocks = set()
         for k, v in goal.items():
             if v != 'table':
                 unique_blocks.add(v) 
             if k[0] != 'table':
                 unique_blocks.add(k[0])
-       
+        
+        return unique_blocks
 
-        goal = {k[0]: v for k, v in goal.items()}
-
-        result = dict()
-        for block in unique_blocks:
+    def _calculate_columns(self, goal) -> Dict[str, int]:
+        processed_goal = {k[0]: v for k, v in goal.items()}
+        result = {}
+        for block in self._get_unique_blocks(goal):
             curr = block
-            deepness = 0
+            depth = 0
 
-            while curr in goal and goal[curr] != 'table':
-                curr = goal[curr]
-                deepness += 1
+            while curr in processed_goal and processed_goal[curr] != 'table':
+                curr = processed_goal[curr]
+                depth += 1
 
-            result[block] = deepness
+            result[block] = depth
 
         return result
-        # TODO:
-        # return a dict of form:
-        # { <block name> : <index of column in the goal state> }
 
 
     def _calculate_fundaments(self, goal) -> Dict[str, List[str]]:
-        # TODO:
-        # return a dict of form:
-        # { <block name> : <list of the blocks below it in the goal state> }
-        unique_blocks = set()
-        for k, v in goal.items():
-            if v != 'table':
-                unique_blocks.add(v) 
-            if k[0] != 'table':
-                unique_blocks.add(k[0])   
-
-        goal = {k[0]: v for k, v in goal.items()}
-
-        result = dict()
-        for block in unique_blocks:
+        processed_goal = {k[0]: v for k, v in goal.items()}
+        result = {}
+        for block in self._get_unique_blocks(goal):
             curr = block
-            temp = list()
+            fundament = []
 
-            while curr in goal and goal[curr] != 'table':
-                curr = goal[curr]
-                temp.append(curr)
+            while curr in processed_goal and processed_goal[curr] != 'table':
+                curr = processed_goal[curr]
+                fundament.append(curr)
 
-            result[block] = temp
+            result[block] = fundament
 
         return result
 
@@ -70,28 +56,19 @@ class BlocksWorldHeuristic():
         state_fundaments = self._calculate_fundaments(temp)
 
         result = 0
-        checked = list()
+        checked = []
 
-        for element, expectedColumn in self.expected_columns.items():
-            if element not in state_columns:
-                print(state)
-                print(state_columns)
-
-
-            if expectedColumn != state_columns[element]:
+        for element, expected_column in self.expected_columns.items():
+            if expected_column != state_columns[element]:
                 result += 1
                 checked.append(element)
 
-        for element, expectedFundaments in self.expected_fundaments.items():
+        for element, expected_fundaments in self.expected_fundaments.items():
             if element in checked:
                 continue
 
-            for i in range(len(expectedFundaments)):
-                if expectedFundaments[i] != state_fundaments[element][i]:
+            for i, value in enumerate(expected_fundaments):
+                if value != state_fundaments[element][i]:
                     result += 2
 
         return result
-        # TODO:
-        # - add 1 to the heuristic value per each block placed in an incorrect column
-        # - for other blocks, add 2 if their fundament is incorrect 
-        # tip. use self.expected_columns and self.expected_fundaments
